5246cb818f243519a4718a6b8819ac0d907f36ae
Sora <bryan.bouillot@epitech.eu>
2021-03-04 16:48:00 +0100

    Correction

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,12 +6,112 @@ import history from '../History.js';
 export default class Home extends Component {
     constructor (props) {
         super (props);
-        this.state = {};
+        this.state = {
+            email: "",
+            prenom: "",
+            nom:"",
+            age: undefined,
+            upPrenom: "",
+            upNom: "",
+            upAge: "",
+        };
+        this.HandleChange = this.HandleChange.bind(this);
+        this.SubmitChange = this.SubmitChange.bind(this);
+        this.DeleteAccount = this.DeleteAccount.bind(this);
+    }
+
+    HandleChange (e) {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    componentDidMount () {
+        const token = localStorage.getItem("token");
+
+        Axios.get(API_BASE_URL + '/api/me', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            this.setState({
+                prenom: res.data.firstname,
+                nom: res.data.lastname,
+                age: res.data.age,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            history.push('/');
+        })
+    }
+
+    Deconnexion () {
+        localStorage.removeItem("token");
+        history.push('/');
+    }
+
+    SubmitChange (e) {
+        e.preventDefault();
+        const token = localStorage.getItem("token");
+        Axios.post(API_BASE_URL + '/api/update', {
+            firstname: this.state.upPrenom,
+            lastname: this.state.upNom,
+            age: this.state.upAge,
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            this.setState({
+                prenom: this.state.upPrenom,
+                nom: this.state.upNom,
+                age: this.state.upAge
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    DeleteAccount () {
+        if(window.confirm("Voulez vous vraiment supprimer votre compte ?")) {
+            const token = localStorage.getItem("token");
+
+            Axios.post(API_BASE_URL + '/api/delete', {}, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+            .then ((res) => {
+                localStorage.removeItem("token");
+                history.push("/");
+            })
+            .catch(err => {console.log(err)});
+        }
     }
 
     render () {
         return (
-            <div></div>
+            <div>
+                <h1> {this.state.prenom} {this.state.nom} </h1>
+                <button type="button" onClick={this.Deconnexion}> Se déconnecter</button>
+
+                <form onSubmit={this.SubmitChange}>
+                    <input type="text" value={this.state.upPrenom} onChange={this.HandleChange} name="upPrenom" placeholder="Prénom" required/>
+                    <input type="text" value={this.state.upNom} onChange={this.HandleChange} name="upNom" placeholder="nom" required/>
+                    <input type="number" min="0" value={this.state.upAge} onChange={this.HandleChange} name="upAge" placeholder="age" required/>
+                    <button type="submit"> Modifier </button>
+                </form>
+
+                <button type="button" onClick={this.DeleteAccount}> Supprimer son Compte</button>
+            </div>
         )
     }
 }
--- src/Components/Log.jsx
+++ src/Components/Log.jsx
@@ -7,12 +7,129 @@ import history from '../History.js';
 export default class Log extends Component {
     constructor (props) {
         super(props);
-        this.state = {};
+        this.state = {
+            email: "",
+            signupEmail: "",
+            password: "",
+            signupPassword: "",
+            prenom: "",
+            nom: "",
+            age: 0,
+            signup: false,
+        };
+        this.HandleChange = this.HandleChange.bind(this);
+        this.HandleSubmit = this.HandleSubmit.bind(this);
+        this.HandleConnexion = this.HandleConnexion.bind(this);
+        this.Display = this.Display.bind(this);
     }
+
+    HandleChange (e) {
+        const { name , value } = e.target;
+
+        this.setState({
+            [name]: value,
+        })
+    }
+    HandleSubmit (e) {
+        e.preventDefault();
+        Axios.post(API_BASE_URL + "/api/signup", {
+            email: this.state.signupEmail,
+            password: this.state.signupPassword,
+            firstname: this.state.prenom,
+            lastname: this.state.nom,
+            age: this.state.age,
+        })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    HandleConnexion (e) {
+        e.preventDefault();
+        Axios.post(API_BASE_URL + '/api/login', {
+            email: this.state.email,
+            password: this.state.password,
+        })
+        .then((res) => {
+            console.log(res);
+            localStorage.setItem("token", res.data.data.token);
+            history.push("/Home");
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    Display () {
+        if (this.state.signup) {
+            return (
+                <form onSubmit={this.HandleSubmit}>
+                    <input type="text" value={this.state.signupEmail} onChange={this.HandleChange} name="signupEmail" required />
+                    <input type="password" value={this.state.signupPassword} onChange={this.HandleChange} name="signupPassword" placeholder="Mot de passe" required/>
+                    <input type="number" min="0" value={this.state.age} onChange={this.HandleChange} name="age" placeholder="age" required/>
+                    <input type="text" value={this.state.prenom} onChange={this.HandleChange} name="prenom" placeholder="Prénom" required/>
+                    <input type="text" value={this.state.nom} onChange={(e) => this.setState({nom: e.target.value})} name="nom" placeholder="nom" required/>
+                    <button type="submit"> S'inscrire </button>
+                    <button type="button" onClick={(e) => {this.setState({signup: !this.state.signup})}}> Se Connecter</button>
+                </form>
+            )
+        } else {
+            return (
+                <form onSubmit={this.HandleConnexion}>
+                    <input type="text" value={this.state.email} onChange={this.HandleChange} name="email" required />
+                    <input type="password" value={this.state.password} onChange={this.HandleChange} name="password" placeholder="Mot de passe" required/>
+                    <button type="submit"> Se Connecter </button>
+                    <button type="button" onClick={(e) => {this.setState({signup: !this.state.signup})}}> S'Inscrire</button>
+                </form>
+            )
+        }
+        return null;
+    }
+
+    componentDidMount () {
+        const token = localStorage.getItem("token");
+
+        if (!token)
+            return;
+        Axios.get(API_BASE_URL + '/api/me', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            history.push("/Home");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
 
     render () {
         return (
-            <div></div>
+            <div>
+                {this.state.signup ?
+                    <form onSubmit={this.HandleSubmit}>
+                        <input type="text" value={this.state.signupEmail} onChange={this.HandleChange} name="signupEmail" required />
+                        <input type="password" value={this.state.signupPassword} onChange={this.HandleChange} name="signupPassword" placeholder="Mot de passe" required/>
+                        <input type="number" min="0" value={this.state.age} onChange={this.HandleChange} name="age" placeholder="age" required/>
+                        <input type="text" value={this.state.prenom} onChange={this.HandleChange} name="prenom" placeholder="Prénom" required/>
+                        <input type="text" value={this.state.nom} onChange={(e) => this.setState({nom: e.target.value})} name="nom" placeholder="nom" required/>
+                        <button type="submit"> S'inscrire </button>
+                        <button type="button" onClick={(e) => {this.setState({signup: !this.state.signup})}}> Se Connecter</button>
+                    </form> :
+                    <form onSubmit={this.HandleConnexion}>
+                        <input type="text" value={this.state.email} onChange={this.HandleChange} name="email" required />
+                        <input type="password" value={this.state.password} onChange={this.HandleChange} name="password" placeholder="Mot de passe" required/>
+                        <button type="submit"> Se Connecter </button>
+                        <button type="button" onClick={(e) => {this.setState({signup: !this.state.signup})}}> S'Inscrire</button>
+                    </form>
+                }
+            </div>
         );
     }
 }
